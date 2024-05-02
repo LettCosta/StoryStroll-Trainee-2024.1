646df826aa99397c88b08979fbf6b103003df771
lelelol <leandrofortunatojf@gmail.com>
2024-05-02 03:22:10 -0300

    adiciona nome da imagem quando ela é upada

---
+++ public/js/add-post.js
@@ -0,0 +1,8 @@
+
+function verifica(){
+    document.getElementById("img").value = null;
+    document.getElementById("img").onchange = function(){
+        document.getElementById("imgname").innerHTML=document.getElementById("img").value;
+    }
+}
+
